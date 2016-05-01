29d93700dd0e96ea0aa6748dd8849d22477f594a
crazy-max <webmaster@crazyws.fr>
2016-05-01 04:20:27 +0200

    User and password is now required  (default user is root and empty password)

--- bin/adminer4.2.4/plugins/login-servers.php
+++ bin/adminer4.2.4/plugins/login-servers.php
@@ -1,7 +1,7 @@
 <?php
 
 /** Display constant list of servers in login form
-* @link http://www.adminer.org/plugins/#use
+* @link https://www.adminer.org/plugins/#use
 * @author Jakub Vrana, http://www.vrana.cz/
 * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
 * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
@@ -14,7 +14,7 @@ class AdminerLoginServers {
 	* @param array array($domain) or array($domain => $description) or array($category => array())
 	* @param string
 	*/
-	function AdminerLoginServers($servers, $driver = "server") {
+	function __construct($servers, $driver = "server") {
 		$this->servers = $servers;
 		$this->driver = $driver;
 	}
@@ -38,7 +38,9 @@ class AdminerLoginServers {
 	function loginForm() {
 		?>
 <table cellspacing="0">
-<tr><th><?php echo lang('Server'); ?><td><input type="hidden" id="username" name="auth[username]" value="root"><input type="hidden" name="auth[password]"><input type="hidden" name="auth[driver]" value="<?php echo $this->driver; ?>"><select name="auth[server]"><?php echo optionlist($this->servers, SERVER); ?></select>
+<tr><th><?php echo lang('Server'); ?><td><input type="hidden" name="auth[driver]" value="<?php echo $this->driver; ?>"><select name="auth[server]"><?php echo optionlist($this->servers, SERVER); ?></select>
+<tr><th><?php echo lang('Username'); ?><td><input id="username" name="auth[username]" value="<?php echo h($_GET["username"]);  ?>">
+<tr><th><?php echo lang('Password'); ?><td><input type="password" name="auth[password]">
 </table>
 <p><input type="submit" value="<?php echo lang('Login'); ?>">
 <?php
